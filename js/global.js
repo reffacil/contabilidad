e47330a709e4123d2562b7d8af829ed563927ee7
RAUL SWANSTON <raul@debian-rs.reffacil.com>
2022-11-16 21:03:24 -0500

    Habilitando la app web

---
+++ js/global.js
@@ -0,0 +1,24 @@
+class Xhr{
+    constructor(path){
+        this.xhr = new XMLHttpRequest();
+        this.xhr.open("GET", path, false);
+        this.xhr.send();
+    }
+    // Return the main content HTML response
+    get response(){return this.xhr.response;}
+}
+
+class DotEnv{
+    constructor(path){
+        const env = new Xhr(path).response;
+        let element = env.split(/\r?\n/);
+        let arrayVar = new Array();
+        element.forEach(row => {
+            let str = row.split(/=/);
+            if(str[0]){
+                arrayVar[str[0].trim()] = str[1].trim();
+            }
+        });
+        return arrayVar;
+    }
+}
